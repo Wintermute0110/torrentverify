e3c5be588844b5dd8984317f9bd7962b6530f2c0
Wintermute0110 <wintermute0110@gmail.com>
2015-06-20 02:06:33 +0900

    Bencodepy module is no longer required

--- bencodepy/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Eric Weast'
-__copyright__ = "Copyright 2014, Eric Weast"
-__license__ = "GPL v2"
-
-from .exceptions import EncodingError
-from .exceptions import DecodingError
-from .decoder import decode
-from .decoder import decode_from_file
-from .encode import encode
