9c1d66af9e9cb73c77705fab565fefbd4e16c454
Wintermute0110 <wintermute0110@gmail.com>
2015-06-20 02:05:44 +0900

    Copy the bdecoder from bencodepy. Removed the requirement for the module collections.abc so it should work on any Python3 version now. Previously, Python 3.3 or higher was required.

--- torrentverify.py
+++ torrentverify.py
@@ -20,13 +20,17 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+#
+# Bencoder code based on Bencodepy by Eric Weast (c) 2014
+# Licensed under the GPL v2
+# https://github.com/eweast/BencodePy/commits/master
 import io
 import sys
 import os
 import hashlib
 import argparse
 import shutil
-import bencodepy
+from collections import OrderedDict
 
 # --- Global variables
 __software_version = '0.1.0';
@@ -54,6 +58,96 @@ class Torrent:
 # https://docs.python.org/3/library/shutil.html#querying-the-size-of-the-output-terminal
 __cols, __lines = shutil.get_terminal_size()
 # print('{0} cols and {1} lines'.format(__cols, __lines))
+
+# --- Bdecoder ----------------------------------------------------------------
+class DecodingError(Exception):
+  def __init__(self, msg):
+    self.msg = msg
+
+  def __str__(self):
+    return repr(self.msg)
+
+class Decoder:
+  def __init__(self, data: bytes):
+    self.data = data
+    self.idx = 0
+
+  def __read(self, i: int) -> bytes:
+    """Returns a set number (i) of bytes from self.data."""
+    b = self.data[self.idx: self.idx + i]
+    self.idx += i
+    if len(b) != i:
+      raise DecodingError(
+         "Incorrect byte length returned between indexes of {0} and {1}. Possible unexpected End of File."
+         .format(str(self.idx), str(self.idx - i)))
+    return b
+
+  def __read_to(self, terminator: bytes) -> bytes:
+    """Returns bytes from self.data starting at index (self.idx) until terminator character."""
+    try:
+      # noinspection PyTypeChecker
+      i = self.data.index(terminator, self.idx)
+      b = self.data[self.idx:i]
+      self.idx = i + 1
+      return b
+    except ValueError:
+      raise DecodingError(
+          'Unable to locate terminator character "{0}" after index {1}.'.format(str(terminator), str(self.idx)))
+
+  def __parse(self) -> object:
+    """Selects the appropriate method to decode next bencode element and returns the result."""
+    char = self.data[self.idx: self.idx + 1]
+    if char in [b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'0']:
+      str_len = int(self.__read_to(b':'))
+      return self.__read(str_len)
+    elif char == b'i':
+      self.idx += 1
+      return int(self.__read_to(b'e'))
+    elif char == b'd':
+      return self.__parse_dict()
+    elif char == b'l':
+      return self.__parse_list()
+    elif char == b'':
+      raise DecodingError('Unexpected End of File at index position of {0}.'.format(str(self.idx)))
+    else:
+      raise DecodingError('Invalid token character ({0}) at position {1}.'.format(str(char), str(self.idx)))
+
+  def decode(self):
+    """Start of decode process. Returns final results."""
+    if self.data[0:1] not in (b'd', b'l'):
+      return self.__wrap_with_tuple()
+    return self.__parse()
+
+  def __wrap_with_tuple(self) -> tuple:
+    """Returns a tuple of all nested bencode elements."""
+    l = list()
+    length = len(self.data)
+    while self.idx < length:
+      l.append(self.__parse())
+    return tuple(l)
+
+  def __parse_dict(self) -> OrderedDict:
+    """Returns an Ordered Dictionary of nested bencode elements."""
+    self.idx += 1
+    d = OrderedDict()
+    key_name = None
+    while self.data[self.idx: self.idx + 1] != b'e':
+      if key_name is None:
+        key_name = self.__parse()
+      else:
+        d[key_name] = self.__parse()
+        key_name = None
+    self.idx += 1
+    return d
+
+  def __parse_list(self) -> list:
+    """Returns an list of nested bencode elements."""
+    self.idx += 1
+    l = []
+    while self.data[self.idx: self.idx + 1] != b'e':
+      l.append(self.__parse())
+    self.idx += 1
+    return l
 
 # --- Functions ---------------------------------------------------------------
 def query_yes_no_all(question, default="no"):
@@ -137,7 +231,9 @@ def extract_torrent_metadata(filename):
   sys.stdout.write('Bdecoding torrent file {0}... '.format(torrentFileName))
   sys.stdout.flush()
   torrent_file = open(torrentFileName, "rb")
-  torr_ordered_dict = bencodepy.decode(torrent_file.read())
+  # Use internal Bdecoder class
+  decoder = Decoder(torrent_file.read())
+  torr_ordered_dict = decoder.decode()
   info_ordered_dict = torr_ordered_dict[b'info']
   sys.stdout.write('done\n')
 
